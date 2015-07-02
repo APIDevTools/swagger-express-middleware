70d1b6b02a13e70b25fae7fa1909aa02a0f02f36
James Messinger <james.messinger@gmail.com>
2015-07-01 21:22:19 -0500

    Set test timeouts

--- tests/test-environment.js
+++ tests/test-environment.js
@@ -22,6 +22,16 @@ global._ = require('lodash');
 // Disable warnings, which clutter the test output
 process.env.WARN = 'off';
 
+// Increase test timeouts.
+beforeEach(function() {
+  // Some of our tests simulate uploading several large files, which takes time.
+  // A lot of this time is garbage-collection between requests
+  this.currentTest.timeout(15000);
+
+  // Almost all of our tests take ~200 ms, since they're simulating full round-trips
+  this.currentTest.slow(2000);
+});
+
 var petStoreJSON = require('./files/petstore.json');
 
 var env = module.exports = {
