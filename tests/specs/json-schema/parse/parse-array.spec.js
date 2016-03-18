2ccb70876ca1ecca8af7c886d4db2b6e282ce30e
James Greene <James.Greene@viavisolutions.com>
2016-03-18 09:38:59 -0500

    Allow empty arrays for non-required parameters
    Fixes #44

--- tests/specs/json-schema/parse/parse-array.spec.js
+++ tests/specs/json-schema/parse/parse-array.spec.js
@@ -272,7 +272,7 @@ describe('JSON Schema - parse array params', function() {
     }
   );
 
-  it('should throw an error if the value is blank',
+  it('should return an empty array if there is no default value and the specified value is blank',
     function(done) {
       var schema = {
         type: 'array',
@@ -283,10 +283,8 @@ describe('JSON Schema - parse array params', function() {
 
       var express = helper.parse(schema, '', done);
 
-      express.use('/api/test', helper.spy(function(err, req, res, next) {
-        expect(err).to.be.an.instanceOf(Error);
-        expect(err.status).to.equal(400);
-        expect(err.message).to.contain('The "Test" header parameter is invalid');
+      express.use('/api/test', helper.spy(function(req, res, next) {
+        expect(req.header('Test')).to.deep.equal([]);
       }));
     }
   );
