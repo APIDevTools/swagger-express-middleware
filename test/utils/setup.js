b05b58239222ffba4d88f6429618715ef734659b
James Messinger <james.messinger@gmail.com>
2019-03-13 07:15:48 -0500

    Replaced "mocha.opts" with "mocharc.yml"

--- test/utils/setup.js
+++ test/utils/setup.js
@@ -6,13 +6,3 @@ chai.use(require("chai-datetime"));
 
 // Disable warnings, which clutter the test output
 process.env.WARN = "off";
-
-// Increase test timeouts.
-beforeEach(function () {
-  // Some of our tests simulate uploading several large files, which takes time.
-  // A lot of this time is garbage-collection between requests
-  this.currentTest.timeout(15000);
-
-  // Almost all of our tests take ~200 ms, since they're simulating full round-trips
-  this.currentTest.slow(2000);
-});
