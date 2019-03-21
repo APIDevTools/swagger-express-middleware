b9701dd9951e44362c64732888b7c4090c106204
James Messinger <james.messinger@gmail.com>
2019-03-21 05:56:18 -0500

    temporarily commented-out placeholder test code

--- test/specs/middleware/validate-request/validate-content-length.spec.js
+++ test/specs/middleware/validate-request/validate-content-length.spec.js
@@ -1,13 +1,13 @@
-"use strict";
+// "use strict";
 
-const _ = require("lodash");
-const sinon = require("sinon");
-const createMiddleware = require("../../../../lib");
-const { expect } = require("chai");
-const fixtures = require("../../../utils/fixtures");
-const { helper } = require("../../../utils");
-const { initTest } = require("./utils");
+// const _ = require("lodash");
+// const sinon = require("sinon");
+// const createMiddleware = require("../../../../lib");
+// const { expect } = require("chai");
+// const fixtures = require("../../../utils/fixtures");
+// const { helper } = require("../../../utils");
+// const { initTest } = require("./utils");
 
-describe.skip("Validate Request middleware - 411 (Length Required)", () => {
+// describe.skip("Validate Request middleware - 411 (Length Required)", () => {
 
-});
+// });
