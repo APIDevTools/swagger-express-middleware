0d3baff8f2d4e82d85fc2bb22624e516dce6e65d
James Messinger <james.messinger@gmail.com>
2019-03-27 04:44:29 -0500

    Fixed the header param parser tests

--- test/specs/middleware/parse-request/header-params.spec.js
+++ test/specs/middleware/parse-request/header-params.spec.js
@@ -6,7 +6,7 @@ const { assert, expect } = require("chai");
 const fixtures = require("../../../utils/fixtures");
 const { helper } = require("../../../utils");
 
-describe.skip("Parse Request middleware - header params", () => {
+describe("Parse Request middleware - header params", () => {
   let api;
 
   beforeEach(() => {
@@ -51,8 +51,8 @@ describe.skip("Parse Request middleware - header params", () => {
       express.get("/api/pets", helper.spy((req, res, next) => {
         expect(req.headers.age).to.equal(4);
         expect(req.header("Age")).to.equal(4);
-        expect(req.headers.tags).to.have.same.members(["big", "brown"]);
-        expect(req.header("Tags")).to.have.same.members(["big", "brown"]);
+        expect(req.headers.tags).to.deep.equal(["big", "brown"]);
+        expect(req.header("Tags")).to.deep.equal(["big", "brown"]);
         expect(req.headers.type).to.be.undefined;
         expect(req.header("Type")).to.be.undefined;
       }));
@@ -72,8 +72,8 @@ describe.skip("Parse Request middleware - header params", () => {
       express.get("/api/pets", helper.spy((req, res, next) => {
         expect(req.headers.age).to.equal(4);
         expect(req.header("Age")).to.equal(4);
-        expect(req.headers.tags).to.have.same.members(["big", 'Fido the "wonder" dog', "brown"]);
-        expect(req.header("Tags")).to.have.same.members(["big", 'Fido the "wonder" dog', "brown"]);
+        expect(req.headers.tags).to.deep.equal(["big", 'Fido the "wonder" dog', "brown"]);
+        expect(req.header("Tags")).to.deep.equal(["big", 'Fido the "wonder" dog', "brown"]);
         expect(req.headers.type).to.be.undefined;
         expect(req.header("Type")).to.be.undefined;
       }));
@@ -100,9 +100,9 @@ describe.skip("Parse Request middleware - header params", () => {
   });
 
   it("should set header params to their defaults if unspecified", (done) => {
-    _.find(api.paths["/pets"].get.parameters, { name: "Age" }).default = 99;
-    _.find(api.paths["/pets"].get.parameters, { name: "Tags" }).default = "hello,world";
-    _.find(api.paths["/pets"].get.parameters, { name: "Type" }).default = "hello world";
+    _.find(api.paths["/pets"].get.parameters, { name: "Age" }).schema.default = 99;
+    _.find(api.paths["/pets"].get.parameters, { name: "Tags" }).schema.default = "hello,world";
+    _.find(api.paths["/pets"].get.parameters, { name: "Type" }).schema.default = "hello world";
 
     createMiddleware(api, (err, middleware) => {
       let express = helper.express(middleware.metadata(), middleware.parseRequest());
@@ -114,8 +114,8 @@ describe.skip("Parse Request middleware - header params", () => {
       express.get("/api/pets", helper.spy((req, res, next) => {
         expect(req.headers.age).to.equal(99);
         expect(req.header("Age")).to.equal(99);
-        expect(req.headers.tags).to.have.same.members(["hello", "world"]);
-        expect(req.header("Tags")).to.have.same.members(["hello", "world"]);
+        expect(req.headers.tags).to.deep.equal(["hello", "world"]);
+        expect(req.header("Tags")).to.deep.equal(["hello", "world"]);
         expect(req.headers.type).to.equal("hello world");
         expect(req.header("Type")).to.equal("hello world");
       }));
@@ -137,31 +137,8 @@ describe.skip("Parse Request middleware - header params", () => {
 
       express.use("/api/pets", helper.spy((err, req, res, next) => {
         expect(err).to.be.an.instanceOf(Error);
-        expect(err.message).to.contain('The "Age" header parameter is invalid ("big,brown")');
-      }));
-    });
-  });
-
-  it("should throw an HTTP 411 error if the Content-Length header is required and is missing", (done) => {
-    let api = _.cloneDeep(fixtures.data.petStore);
-    api.paths["/pets"].post.parameters.push({
-      in: "header",
-      name: "Content-Length",
-      required: true,
-      type: "integer"
-    });
-
-    createMiddleware(api, (err, middleware) => {
-      let express = helper.express(middleware.metadata(), middleware.parseRequest());
-
-      helper.supertest(express)
-        .post("/api/pets")
-        .set("content-length", "")
-        .end(helper.checkSpyResults(done));
-
-      express.use("/api/pets", helper.spy((err, req, res, next) => {
-        expect(err.status).to.equal(411);
-        expect(err.message).to.contain('Missing required header parameter "Content-Length"');
+        expect(err.message).to.equal(
+          'The "Age" header parameter is invalid. "big,brown" is not a valid numeric value');
       }));
     });
   });
--- test/specs/middleware/validate-request/validate-content-length.spec.js
+++ test/specs/middleware/validate-request/validate-content-length.spec.js
@@ -10,4 +10,28 @@
 
 // describe.skip("Validate Request middleware - 411 (Length Required)", () => {
 
+//   it("should throw an HTTP 411 error if the Content-Length header is required and is missing", (done) => {
+//     let api = _.cloneDeep(fixtures.data.petStore);
+//     api.paths["/pets"].post.parameters.push({
+//       in: "header",
+//       name: "Content-Length",
+//       required: true,
+//       type: "integer"
+//     });
+
+//     createMiddleware(api, (err, middleware) => {
+//       let express = helper.express(middleware.metadata(), middleware.parseRequest());
+
+//       helper.supertest(express)
+//         .post("/api/pets")
+//         .set("content-length", "")
+//         .end(helper.checkSpyResults(done));
+
+//       express.use("/api/pets", helper.spy((err, req, res, next) => {
+//         expect(err.status).to.equal(411);
+//         expect(err.message).to.equal('Missing required header parameter "Content-Length"');
+//       }));
+//     });
+//   });
+
 // });
